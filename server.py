f56d495199226ab3793147531f968ae30bfee10a
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-17 11:53:33 +0100

    Added TCP server initial code

---
+++ server.py
@@ -0,0 +1,35 @@
+import socket
+
+# Create the socket object
+server_socket = socket.socket(
+    socket.AF_INET, # Using IPv4
+    socket.SOCK_STREAM # Connection based protocol
+)
+
+# IP address of port
+host = socket.gethostbyname()
+
+# Port to listen on
+port = 444
+
+# Bind the host and port to the socket object
+server_socket.bind(host, port)
+
+# Number of requests allowed at a given time
+server_socket.listen(3)
+
+while True:
+    # Allow incoming TCP connections from the client
+    client_socket, address = server_socket.accept()
+
+    # Console logging
+    print(f"Recieved connection from ${address}")
+
+    # Welcome message upon connecting to the TCP server
+    message = "Hello! Thank you for connecting to the server" + "\r\n"
+
+    # Send the welcome message to the client
+    client_socket.send(message)
+
+    # Close the TCP connection
+    client_socket.close()
